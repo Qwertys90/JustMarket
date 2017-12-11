f8aa20e04d49a7697e17a9787ef79b08b2ff0b26
qwertys90 <mr.kris90@gmail.com>
2017-12-11 11:21:34 +0100

    1st commit

---
+++ src/main/java/it/cgl/justmarket/JustMarketApplication.java
@@ -0,0 +1,12 @@
+package it.cgl.justmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JustMarketApplication.class, args);
+	}
+}
